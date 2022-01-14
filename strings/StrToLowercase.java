3dde0f8e775eacdd14c0d14dab60bd2e78c1c698
mounikayedla <89407537+mounikayedla@users.noreply.github.com>
2022-01-14 10:55:30 +0530

    Add files via upload

---
+++ strings/StrToLowercase.java
@@ -0,0 +1,13 @@
+package assignmentjava1;
+
+public class StrToLowercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str = "Java String pool refers to collection of Strings which are stored in heap memory";
+	        String result = str.toLowerCase();
+	        System.out.println("Original String  : " + str);
+	        System.out.println("Lowercase String : " + result);
+	}
+
+}
