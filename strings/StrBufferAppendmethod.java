74a487a43661fad31400fd991b3505247f0c1df6
mounikayedla <89407537+mounikayedla@users.noreply.github.com>
2022-01-14 10:57:11 +0530

    Add files via upload

---
+++ strings/StrBufferAppendmethod.java
@@ -0,0 +1,14 @@
+package assignmentjava1;
+
+public class StrBufferAppendmethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 StringBuffer s = new StringBuffer("StringBuffer");
+		 s.append(" is a peer class of String");
+		 s.append(" that provides much of");
+		 s.append(" the functionality of Strings");
+		 System.out.println(s);
+	}
+
+}
