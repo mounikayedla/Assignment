c51c73fafa2d0c4f197d0b6b3dda1a16b3853eb2
mounikayedla <89407537+mounikayedla@users.noreply.github.com>
2022-01-14 10:58:07 +0530

    Add files via upload

---
+++ StrBufferInsertmethod.java
@@ -0,0 +1,12 @@
+package assignmentjava1;
+
+public class StrBufferInsertmethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  StringBuffer s = new StringBuffer("It is used to _ at the specified index position");
+	     s.insert(15, "insert text");
+	    
+	     System.out.println(s);
+	}
+}
