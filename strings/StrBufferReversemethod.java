babb01b72aea5fa78d16bc3f91347ea9819bea46
mounikayedla <89407537+mounikayedla@users.noreply.github.com>
2022-01-14 10:58:54 +0530

    Add files via upload

---
+++ strings/StrBufferReversemethod.java
@@ -0,0 +1,13 @@
+package assignmentjava1;
+
+public class StrBufferReversemethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  StringBuffer s = new StringBuffer("This method returns the reversed object on which it was called");  
+	        System.out.println("string: " + s);  
+	        
+	        System.out.println("reverse: " + s.reverse());  
+	}
+
+}
