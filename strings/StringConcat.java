f0d000c2a28317b6831986ffb24eb98216098493
mounikayedla <89407537+mounikayedla@users.noreply.github.com>
2022-01-14 10:56:36 +0530

    Add files via upload

---
+++ strings/StringConcat.java
@@ -0,0 +1,15 @@
+package assignmentjava1;
+
+public class StringConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*String str="Hello"+" How are you";
+System.out.println(str);*/
+		String s1="Hello";
+		String s2=" How are you";
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+	}
+
+}
